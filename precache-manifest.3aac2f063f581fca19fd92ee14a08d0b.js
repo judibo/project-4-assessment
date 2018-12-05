4b2b5348dedc9c2440fd2993f44c7db372032025
Julia Dibo <judibo@Julias-MacBook-Air.local>
2018-12-05 14:28:53 -0800

    Updates

---
+++ precache-manifest.3aac2f063f581fca19fd92ee14a08d0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee902e61a5a89ae0d65b",
+    "url": "/project-4-assessment/static/js/runtime~main.ee902e61.js"
+  },
+  {
+    "revision": "242831f4b20081d8c518",
+    "url": "/project-4-assessment/static/js/main.242831f4.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/project-4-assessment/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "242831f4b20081d8c518",
+    "url": "/project-4-assessment/static/css/main.14c4b3ca.chunk.css"
+  },
+  {
+    "revision": "4d8fb6cf7eb077fa46d2f2ff9f52f238",
+    "url": "/project-4-assessment/index.html"
+  }
+];
